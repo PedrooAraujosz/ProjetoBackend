4c312f7ca86c660cac434c86047b52c8b70d8a9f
PedrooAraujosz <mxpedro.araujo11@gmail.com>
2024-11-19 11:11:58 -0300

    Projetobackend

--- ProjetoBackend/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ ProjetoBackend/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -64,23 +64,23 @@ namespace ProjetoBackend.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Required]
-            [EmailAddress]
+            [Required(ErrorMessage = "O Campo E-mail é Obrigatório!")]
+            [EmailAddress(ErrorMessage = "O Formato do Email é Inválido!")]
             public string Email { get; set; }
 
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Required]
-            [DataType(DataType.Password)]
+            [Required(ErrorMessage = "O Campo Senha é Obrigatório!")]
+            [DataType(DataType.Password, ErrorMessage = "Formato da Senha é Invalida!")]
             public string Password { get; set; }
 
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Lembre-me?")]
             public bool RememberMe { get; set; }
         }
 
@@ -114,7 +114,7 @@ namespace ProjetoBackend.Areas.Identity.Pages.Account
                 var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
-                    _logger.LogInformation("User logged in.");
+                    _logger.LogInformation("Logado.");
                     return LocalRedirect(returnUrl);
                 }
                 if (result.RequiresTwoFactor)
@@ -123,12 +123,12 @@ namespace ProjetoBackend.Areas.Identity.Pages.Account
                 }
                 if (result.IsLockedOut)
                 {
-                    _logger.LogWarning("User account locked out.");
+                    _logger.LogWarning("Usuário Bloqueado.");
                     return RedirectToPage("./Lockout");
                 }
                 else
                 {
-                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    ModelState.AddModelError(string.Empty, "Tentativa de Login Inválida.");
                     return Page();
                 }
             }
